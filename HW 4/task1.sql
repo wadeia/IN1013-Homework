ae2f0349bf86ea8cad7216c297aa3da067e70f8e
wadeia <wadeia.naz@city.ac.uk>
2024-10-27 20:16:53 +0000

    Add files via upload

---
+++ HW 4/task1.sql
@@ -0,0 +1,65 @@
+-- create a t-- DROP DATABASE IF EXISTS pet_database;
+-- CREATE DATABASE pet_database;
+-- USE pet_database;
+
+-- DROP TABLE IF EXISTS petPet, petEvent;
+
+-- Up to the next CUTOFF POINT should be in task1.sql
+
+petPet (
+  petname Fluffy,
+  owner Diane,
+  species Cat,
+  gender F,
+  birth 2020-10-15,
+  death STILL DIDNT DIE,
+);
+
+??? petEvent (
+  petname Hammy,
+  eventdate 1995-05-15,
+  eventtype litter,
+  remark 4 kittens, 3 female, 1 male,
+  FOREIGN KEY Fluffy,
+  PRIMARY KEY F
+);
+
+------------------------- CUTOFF POINT ------------------------------
+
+INSERT INTO petPet VALUES
+    ("Fluffy", "Harold", "cat", "F", "1993-02-04", NULL),
+    ("Claws", "Gwen", "cat", "M", "1994-03-17", NULL),
+    ("Buffy", "Harold", "dog", "F", "1989-05-13", NULL),
+    ("Fang", "Benny", "dog", "M", "1990-08-27", NULL),
+    ("Bowser", "Diane", "dog", "M", "1979-08-31", "1995-07-29"),
+    ("Chirpy", "Gwen", "bird", "F", "1998-09-11", NULL),
+    ("Whistler", "Gwen", "bird", NULL, "1997-12-09", NULL),
+    ("Slim", "Benny", "snake", "M", "1996-04-29", NULL),
+    ('Puffball','Diane','hamster','F','1999-03-30',NULL);
+
+INSERT INTO petEvent VALUES
+    ("Fluffy", "1995-05-15", "litter", "4 kittens, 3 female, 1 male"),
+    ("Buffy", "1993-06-23", "litter", "5 puppies, 2 female, 3 male"),
+    ("Buffy", "1994-06-19", "litter", "3 puppies, 3 female"),
+    ("Chirpy", "1999-03-21", "vet", "needed beak straightened"),
+    ("Slim", "1997-08-03", "vet", "broken rib"),
+    ("Slim", "1997-10-04", "vet", "broken tooth"),
+    ("Bowser", "1991-10-12", "kennel", NULL),
+    ("Fang", "1991-10-12", "kennel", NULL),
+    ("Fang", "1998-08-28", "birthday", "Gave him a new chew toy"),
+    ("Claws", "1998-03-17","birthday", "Gave him a new flea collar"),
+    ("Whistler", "1998-12-09", "birthday", "First birthday");
+
+------------------------ CUTOFF POINT ------------------------------
+
+-- Up to the end should be in task2.sqlable
+CREATE TABLE students (
+  id INTEGER PRIMARY KEY,
+  name TEXT NOT NULL,
+  gender TEXT NOT NULL
+);
+-- insert some values
+INSERT INTO students VALUES (1, 'Ryan', 'M');
+INSERT INTO students VALUES (2, 'Joanna', 'F');
+-- fetch some values
+SELECT * FROM students WHERE gender = 'F';
